6d789b415d74fe7df7ca4ba36c5c96caafcc5ef9
Juan Carlos Zuluaga <juan.zuluaga@cscgfm.com>
2023-11-05 18:42:18 -0500

    Separate razor and c#

---
+++ Orders.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Orders.Frontend.Shared
+{
+    public partial class GenericList<Titem>
+    {
+        [EditorRequired]
+        [Parameter]
+        public List<Titem> MyList { get; set; } = null!;
+    }
+}
