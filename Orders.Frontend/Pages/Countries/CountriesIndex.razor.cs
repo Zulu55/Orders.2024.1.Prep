a21470230bd4751e2235c32e447cc5dc03b8e04b
Juan Carlos Zuluaga <juan.zuluaga@cscgfm.com>
2023-11-05 18:35:43 -0500

    Generic list

---
+++ Orders.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+
+        public List<Country>? Countries { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var responseHppt = await Repository.GetAsync<List<Country>>("api/countries");
+            Countries = responseHppt.Response!;
+        }
+    }
+}
