e6e1bcf9e7f93b3cd9de2a416d7a5a6ddff40808
Juan Carlos Zuluaga <juan.zuluaga@cscgfm.com>
2023-11-09 18:22:12 -0500

    Re-send confirmation user email

--- Orders.Backend/Controllers/AccountsController.cs
+++ Orders.Backend/Controllers/AccountsController.cs
@@ -32,6 +32,24 @@ namespace Orders.Backend.Controllers
             _container = "users";
         }
 
+        [HttpPost("ResedToken")]
+        public async Task<IActionResult> ResedToken([FromBody] EmailDTO model)
+        {
+            var user = await _usersUnitOfWork.GetUserAsync(model.Email);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var response = await SendConfirmationEmailAsync(user);
+            if (response.WasSuccess)
+            {
+                return NoContent();
+            }
+
+            return BadRequest(response.Message);
+        }
+
         [HttpGet("ConfirmEmail")]
         public async Task<IActionResult> ConfirmEmailAsync(string userId, string token)
         {
