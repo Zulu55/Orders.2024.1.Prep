e57c669b1ba5ab4bf7bd346b9285577f96176577
Juan Carlos Zuluaga <juan.zuluaga@cscgfm.com>
2023-11-09 14:56:36 -0500

    Drop down cascade

--- Orders.Backend/Repositories/Interfaces/IStatesRepository.cs
+++ Orders.Backend/Repositories/Interfaces/IStatesRepository.cs
@@ -11,5 +11,7 @@ namespace Orders.Backend.Repositories.Interfaces
         Task<ActionResponse<IEnumerable<State>>> GetAsync(PaginationDTO pagination);
 
         Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination);
+
+        Task<IEnumerable<State>> GetComboAsync(int countryId);
     }
 }
--- Orders.Backend/UnitsOfWork/Implementations/StatesUnitOfWork.cs
+++ Orders.Backend/UnitsOfWork/Implementations/StatesUnitOfWork.cs
@@ -15,6 +15,8 @@ namespace Orders.Backend.UnitsOfWork.Implementations
             _statesRepository = statesRepository;
         }
 
+        public async Task<IEnumerable<State>> GetComboAsync(int countryId) => await _statesRepository.GetComboAsync(countryId);
+
         public override async Task<ActionResponse<IEnumerable<State>>> GetAsync(PaginationDTO pagination) => await _statesRepository.GetAsync(pagination);
 
         public override async Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination) => await _statesRepository.GetTotalPagesAsync(pagination);
